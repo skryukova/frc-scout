a803ccfa4e0e8e98ee1c374671af6d9f37da7abc
skryukova <kryukova@hotmail.com>
2018-12-07 09:19:24 -0600

    Basic outline and navigation in place

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CurrentEventComponent } from './current-event/current-event.component'
+import { TeamsComponent } from './teams/teams.component'
 
 const routes: Routes = [
   {
@@ -11,6 +12,10 @@ const routes: Routes = [
   {
     path: 'current-event',
     component: CurrentEventComponent
+  },
+  {
+    path: 'teams',
+    component: TeamsComponent
   }
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { HomeComponent } from './home/home.component';
 import { CurrentEventComponent } from './current-event/current-event.component';
+import { TeamsComponent } from './teams/teams.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     HomeComponent,
-    CurrentEventComponent
+    CurrentEventComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule,
