23350132bdace94856b4ab7e66db8ce98e42b9be
Mark <mbaumste@purdue.edu>
2019-02-22 18:37:40 -0500

    added current event display

--- frontend/src/app/current-event/current-event.component.ts
+++ frontend/src/app/current-event/current-event.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-current-event',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CurrentEventComponent implements OnInit {
 
-  constructor() { }
+  currentEvent : JSON;
+
+  constructor(private httpClient : HttpClient) { }
 
   ngOnInit() {
+  }
+  
+  getCurrentEvent() {
+    this.httpClient.get('http://localhost:5000/events/current').subscribe(data => {
+      this.currentEvent = data as JSON;
+      console.log(this.currentEvent);
+    })
   }
 
 }
