4d1d33924947553bb587df2bea2e2e644ece699d
skryukova <kryukova@hotmail.com>
2019-03-15 13:12:21 -0500

    Initial code to switch to material design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { MatButtonModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -22,6 +25,11 @@ import { MatchTeamReportComponent } from './match-team-report/match-team-report.
     MatchTeamReportComponent
   ],
   imports: [
+    NoopAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
     BrowserModule,
     FormsModule,
     AppRoutingModule,
