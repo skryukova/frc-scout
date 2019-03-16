465b97e3bcdd56ef701667fbb5ebbcc39002b884
skryukova <kryukova@hotmail.com>
2019-03-15 20:23:27 -0500

    Remoted unnecessary todo class

--- backend/frc_scout/__init__.py
+++ backend/frc_scout/__init__.py
@@ -15,18 +15,9 @@ from .datastore import Match
 from .datastore import Event
 from .datastore import EventMatch
 
-todos = {}
 
 from flask import request
 from flask_restful import Resource
-
-class TodoSimple(Resource):
-    def get(self, todo_id):
-        return {todo_id: todos[todo_id]}
-
-    def put(self, todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id: todos[todo_id]}
 
 
 def create_app(test_config=None):
